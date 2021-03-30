2843a3e14f156503c7894b69661d943d2e43db60
Joseph Yu <josephyu2712+github@gmail.com>
2021-03-30 23:08:05 +0100

    Updated routine

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euf -o pipefail
+
+# Perform the actual Python building and installing
+# Ideally we're currently in an empty directory
+INSTALL_DIR="${INSTALL_DIR:-$(mktemp -d)}"
+VERSION="${VERSION:-3.7.10}"
+
+mkdir -vp "$INSTALL_DIR"
+
+CURL_FLAGS=("-L")
+[ -t 1 ] && CURL_FLAGS+=("-#") || CURL_FLAGS+=("-sS")
+
+curl "${CURL_FLAGS[@]}" https://www.python.org/ftp/python/"$VERSION"/Python-"$VERSION".tar.xz \
+| tar --strip-components=1 -xJ
+
+./configure --prefix="$INSTALL_DIR" --with-pydebug --enable-shared
+LD_RUN_PATH='$ORIGIN:$ORIGIN/../lib' make -s -j"$(nproc)"
+make -s -j"$(nproc)" install
+
+# Ensure all libraries are linked properly. Exit 1 if any libraries are "not found"
+LDD_RESULTS="$(mktemp)"
+ldd -v "$INSTALL_DIR"/bin/python"${VERSION%%.*}" > "$LDD_RESULTS"
+grep -q "not found" "$LDD_RESULTS" || exit
+
+grep "not found" "$LDD_RESULTS"
+echo "----- from: ldd -v "$INSTALL_DIR"/bin/python"${VERSION%%.*}" -----"
+cat "$LDD_RESULTS"
+exit 1
--- package.py
+++ package.py
@@ -4,21 +4,30 @@ __version__ = "3.7.10"
 version = __version__
 
 build_command = r"""
-docker build --rm \
-    --build-arg VERSION="{version}" \
-    --build-arg INSTALL_DIR="{install_dir}" \
-    -t local/python \
-    $REZ_BUILD_SOURCE_PATH
+set -eufx -o pipefail
+
+cp "$REZ_BUILD_SOURCE_PATH"/Dockerfile "$REZ_BUILD_SOURCE_PATH"/entrypoint.sh .
+
+IMAGE_ID_FILE="$(readlink -f DockerImageID)"
+docker build --rm --iidfile="$IMAGE_ID_FILE" .
+
+CONTAINER_ARGS=(docker create)
+CONTAINER_ARGS+=("--env" "INSTALL_DIR={install_dir}")
+CONTAINER_ARGS+=("--env" "VERSION={version}")
+CONTAINER_ARGS+=("$(cat $IMAGE_ID_FILE)")
+
 
 if [ $REZ_BUILD_INSTALL -eq 1 ]
 then
-    CONTAINTER_ID=$(docker run --rm -td local/python)
-    docker cp $CONTAINTER_ID:{install_dir}/. {install_dir}
-    docker stop $CONTAINTER_ID
+    CONTAINTER_ID=$("{CONTAINER_ARGS}")
+    docker start -ia "$CONTAINTER_ID"
+    docker cp "$CONTAINTER_ID"/"{install_dir}"/. "{install_dir}"/
+    docker rm "$CONTAINTER_ID"
 fi
 """.format(
     version=__version__,
     install_dir="${{REZ_BUILD_INSTALL_PATH:-/usr/local}}",
+    CONTAINER_ARGS="${{CONTAINER_ARGS[@]}}",
 )
 
 
@@ -26,4 +35,3 @@ def commands():
     import os.path
 
     env.PATH.append(os.path.join("{root}", "bin"))
-    env.LD_LIBRARY_PATH.append(os.path.join("{root}", "lib"))
